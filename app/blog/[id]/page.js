a1c6c2316372790a631875c35fe1a5a2ef823940
0SyedAli <sa8449006@gmail.com>
2024-07-23 05:46:41 +0500

    add api just post method

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Profile = () => {
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return res.json();
+};
+
+const Profile = async () => {
+  const data = await getData();
+  console.log("Data:", data);
   return (
-    <div>Profile page</div>
-  )
-}
+    <>
+      {data.map((post, i) => (
+        <div key={i} className="mt-4 text-center">
+          <h1 className="font-bold fs-2">{post.title}</h1>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
